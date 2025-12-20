fc1ea39ed0e0aabc6b34000571293d0a7655f7fc
Rosyid <habibulloharrosyid@gmail.com>
2025-12-20 10:54:33 +0800

    fix: allow backend & undiksha images

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,13 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "booking-be-production-38e5.up.railway.app",
+        pathname: "/storage/**",
+      },
+      {
+        protocol: "https",
         hostname: "cdn.undiksha.ac.id",
+        pathname: "/**",
       },
     ],
   },
