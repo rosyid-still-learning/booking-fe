ff80d5c085b9d984262f07d51750a5f928716f80
Rosyid <habibulloharrosyid@gmail.com>
2025-12-20 10:06:01 +0800

    fix: remove tailwind v4 and fix postcss config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: false, // ⬅️ PAKSA MATI
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
